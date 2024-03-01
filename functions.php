10010906e1ddc5f9462df90bcbda4e8eba595d11
Abenezer <abenezerasgedom@gmail.com>
2024-03-01 13:56:52 +0300

    Commit

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once('conncection.php');
 
@@ -9,9 +9,42 @@ require_once('conncection.php');
  * @throws Exception if the account information cannot be retrieved
  * @return Account the fetched account
  */
-function fetchAccount(){
+function fetchAccount()
+{
     try {
+        require_once('../config.php');
+        $session = session_id();
 
+        $trueSession = false;
+
+        $UID = 0;
+
+        $sql = "SELECT * FROM mdlwj_sessions WHERE sid = ? AND userid != ?";
+
+        // Prepare the statement
+        $stmt = $conn->prepare($sql);
+
+        // Bind the session ID to the parameter placeholder
+        $stmt->bind_param("si", $session, $UID);
+
+        // Execute the statement
+        $stmt->execute();
+
+        // Fetch the result
+        $stmt->store_result();
+
+        // Check if the value exists
+        if ($stmt->num_rows > 0) {
+
+            $trueSession = true;
+        } else {
+            // No data found in the database, redirect the user to a specific URL
+            $trueSession = false;
+        }
+
+        // Close the statement and database connection
+        $stmt->close();
+        $conn->close();
     } catch (Exception $e) {
         throw new Exception("Account information cannot be retrieved: " . $e->getMessage());
     }
